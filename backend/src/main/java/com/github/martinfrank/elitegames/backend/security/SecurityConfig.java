c424711ff323bc689f5a439c4bde176fcf7a6808
MartinFrank77 <martin.frank@irix.ch>
2025-11-20 11:18:20 +0100

    fixed typo

--- backend/src/main/java/com/github/martinfrank/elitegames/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/github/martinfrank/elitegames/backend/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(alloweddOrigins());
+        configuration.setAllowedOrigins(allowedOrigins());
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
